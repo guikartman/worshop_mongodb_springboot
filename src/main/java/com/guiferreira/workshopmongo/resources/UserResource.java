3b28ee9929f00e27fc6d90814e8115d8db99e74f
Guilherme Dantas <guikartman@gmail.com>
2021-01-11 21:27:06 -0300

    Insercao de usuario com POST

--- src/main/java/com/guiferreira/workshopmongo/resources/UserResource.java
+++ src/main/java/com/guiferreira/workshopmongo/resources/UserResource.java
@@ -1,14 +1,17 @@
 package com.guiferreira.workshopmongo.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.guiferreira.workshopmongo.domain.User;
 import com.guiferreira.workshopmongo.dto.UserDTO;
@@ -39,5 +42,14 @@ public class UserResource {
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		User user = userService.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(user));
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<UserDTO> insert(@RequestBody UserDTO dto) {
+		User user = userService.fromDTO(dto);
+		user = userService.insert(user);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(user.getId()).toUri();
+		return ResponseEntity.created(uri).body(new UserDTO(user));
 	}
 }
--- src/main/java/com/guiferreira/workshopmongo/services/UserService.java
+++ src/main/java/com/guiferreira/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.guiferreira.workshopmongo.domain.User;
+import com.guiferreira.workshopmongo.dto.UserDTO;
 import com.guiferreira.workshopmongo.repository.UserRepository;
 import com.guiferreira.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -25,4 +26,11 @@ public class UserService {
 		return optional.orElseThrow(() -> new ObjectNotFoundException("Usuario com id: "+ id+ " ,não foi encontrado"));
 	}
 
+	public User insert(User user) {
+		return repo.insert(user);
+	}
+	
+	public User fromDTO(UserDTO dto) {
+		return new User(dto.getId(), dto.getName(), dto.getEmail());
+	}
 }
